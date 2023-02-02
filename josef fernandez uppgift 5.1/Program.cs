7846d25a112f2035b4987e3489663e66dd049ca9
Pingvu <nhlpool27@gmail.com>
2023-02-02 13:40:53 +0100

    Add project files.

---
+++ josef fernandez uppgift 5.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace test51
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv in 5 stycken namn");
+            string[] namn = new string[5];
+
+            namn[0] = Console.ReadLine();
+            namn[1] = Console.ReadLine();
+            namn[2] = Console.ReadLine();
+            namn[3] = Console.ReadLine();
+            namn[4] = Console.ReadLine();
+        }
+
+    }
+}
